fa56c8fd3050fb86f1011b2823bb31a0a7e9f98c
Your Name <you@example.com>
2026-07-06 00:46:24 +0530

    Prepare application for production deployment

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,3 +1,20 @@
 from .base import *
 
 DEBUG = False
+
+ALLOWED_HOSTS = [
+    ".onrender.com",
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.onrender.com",
+]
+
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+X_FRAME_OPTIONS = "DENY"
